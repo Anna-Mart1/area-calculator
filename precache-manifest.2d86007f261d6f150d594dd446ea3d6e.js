cd4fd956248a5cdd05982465e21b89c59f4a0566
MartAnna <ms.mart.anna@gmail.com>
2020-07-06 12:58:25 +0300

    Updates

---
+++ precache-manifest.2d86007f261d6f150d594dd446ea3d6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08178036ab6f7f52d328f878cd1f09e6",
+    "url": "/area-calculator/index.html"
+  },
+  {
+    "revision": "d14471a81aa88a27b9d6",
+    "url": "/area-calculator/static/css/main.38c74b7d.chunk.css"
+  },
+  {
+    "revision": "96d6e15ea883422ed75c",
+    "url": "/area-calculator/static/js/2.276d7a91.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/area-calculator/static/js/2.276d7a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d14471a81aa88a27b9d6",
+    "url": "/area-calculator/static/js/main.80488c20.chunk.js"
+  },
+  {
+    "revision": "ceb4962e8df4b6d14e83",
+    "url": "/area-calculator/static/js/runtime-main.8190a10d.js"
+  }
+]);
---
+++ precache-manifest.dbff1893ae64185ec88c320428c35a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4341ae498aaf99f342ebb3a7f8fbe1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299aaf7438bff9046f26",
+    "url": "/static/css/main.72581f3b.chunk.css"
+  },
+  {
+    "revision": "96d6e15ea883422ed75c",
+    "url": "/static/js/2.276d7a91.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.276d7a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299aaf7438bff9046f26",
+    "url": "/static/js/main.47a898a7.chunk.js"
+  },
+  {
+    "revision": "2675c5b354af3a040bf2",
+    "url": "/static/js/runtime-main.447776e0.js"
+  }
+]);
